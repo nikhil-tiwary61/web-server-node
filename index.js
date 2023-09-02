1f1386305b1b75bb8e53edc77f7f6c426ee0de89
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-02 17:50:33 +0530

    Set headers and use request and response

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require("http");
 
+const data = { age: 5 };
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   console.log("server created");
-  res.end("hello");
+  res.setHeader("Dummyheader", "Dummyvalue");
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(data));
 });
 
 server.listen(8080);
