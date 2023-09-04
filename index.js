ecb9fe720211ef26e7a78950a9d7c839da31fd73
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-04 23:50:29 +0530

    Add static file index.html for static hosting

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require("http");
+const fs = require("fs");
+
+const index = fs.readFileSync("index.html", "utf-8");
 
 const data = { age: 5 };
 const server = http.createServer((req, res) => {
   console.log(req.url);
   console.log("server created");
   res.setHeader("Dummyheader", "Dummyvalue");
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
+  // res.setHeader("Content-Type", "application/json");
+  res.end(index);
 });
 
 server.listen(8080);
