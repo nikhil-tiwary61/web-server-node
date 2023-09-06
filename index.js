0d9558898296274e23c560dfdb3f1ca29db77c02
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-06 20:24:08 +0530

    add routes using request url

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
+const data = fs.readFileSync("data.json", "utf-8");
 
-const data = { age: 5 };
 const server = http.createServer((req, res) => {
   console.log(req.url);
+
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(data);
+      break;
+    default:
+      res.writeHead(404);
+      res.end();
+  }
   console.log("server created");
-  res.setHeader("Dummyheader", "Dummyvalue");
-  // res.setHeader("Content-Type", "application/json");
-  res.end(index);
+  // res.setHeader("Dummyheader", "Dummyvalue");
 });
 
 server.listen(8080);
