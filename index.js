acc3d1e1b78bdc45576253b6d1b54c0f1597f87d
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-07 18:19:18 +0530

    made site load dynamic data by matching url params to database

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const product = data.products[0];
+const products = data.products;
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
+
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const product = products.find((p) => p.id === +id);
+    res.setHeader("Content-Type", "text/html");
+    let modifiedProduct = index
+      .replace("**title**", product.title)
+      .replace("**url**", product.thumbnail)
+      .replace("**price**", product.price)
+      .replace("**rating**", product.rating);
+    res.end(modifiedProduct);
+    return;
+  }
 
   switch (req.url) {
     case "/":
@@ -17,15 +30,15 @@ const server = http.createServer((req, res) => {
       res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify(data));
       break;
-    case "/product":
-      res.setHeader("Content-Type", "text/html");
-      let modifiedProduct = index
-        .replace("**title**", product.title)
-        .replace("**url**", product.thumbnail)
-        .replace("**price**", product.price)
-        .replace("**rating**", product.rating);
-      res.end(modifiedProduct);
-      break;
+    // case "/product":
+    //   res.setHeader("Content-Type", "text/html");
+    //   let modifiedProduct = index
+    //     .replace("**title**", product.title)
+    //     .replace("**url**", product.thumbnail)
+    //     .replace("**price**", product.price)
+    //     .replace("**rating**", product.rating);
+    //   res.end(modifiedProduct);
+    //   break;
     default:
       res.writeHead(404);
       res.end();
