6af712a41aed66d54ddc92dab0f65eaaf5139737
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-07 19:05:37 +0530

    initialise server using express

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
-const http = require("http");
 const fs = require("fs");
+const express = require("express");
 
-const index = fs.readFileSync("index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = data.products;
+// const index = fs.readFileSync("index.html", "utf-8");
+// const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+// const products = data.products;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-
-  if (req.url.startsWith("/product")) {
-    const id = req.url.split("/")[2];
-    const product = products.find((p) => p.id === +id);
-    res.setHeader("Content-Type", "text/html");
-    let modifiedProduct = index
-      .replace("**title**", product.title)
-      .replace("**url**", product.thumbnail)
-      .replace("**price**", product.price)
-      .replace("**rating**", product.rating);
-    res.end(modifiedProduct);
-    return;
-  }
-
-  switch (req.url) {
-    case "/":
-      res.setHeader("Content-Type", "text/html");
-      res.end(index);
-      break;
-    case "/api":
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(data));
-      break;
-    // case "/product":
-    //   res.setHeader("Content-Type", "text/html");
-    //   let modifiedProduct = index
-    //     .replace("**title**", product.title)
-    //     .replace("**url**", product.thumbnail)
-    //     .replace("**price**", product.price)
-    //     .replace("**rating**", product.rating);
-    //   res.end(modifiedProduct);
-    //   break;
-    default:
-      res.writeHead(404);
-      res.end();
-  }
-  console.log("server created");
-  // res.setHeader("Dummyheader", "Dummyvalue");
+const server = express();
+server.listen(8080, () => {
+  console.log("server started");
 });
-
-server.listen(8080);
