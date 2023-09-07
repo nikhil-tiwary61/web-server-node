3d96556ea3cc2f136fc58630616b4db2136fb2f1
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-07 16:18:52 +0530

    add data dynamically into the /product route

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
-const data = fs.readFileSync("data.json", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const product = data.products[0];
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -14,7 +15,16 @@ const server = http.createServer((req, res) => {
       break;
     case "/api":
       res.setHeader("Content-Type", "application/json");
-      res.end(data);
+      res.end(JSON.stringify(data));
+      break;
+    case "/product":
+      res.setHeader("Content-Type", "text/html");
+      let modifiedProduct = index
+        .replace("**title**", product.title)
+        .replace("**url**", product.thumbnail)
+        .replace("**price**", product.price)
+        .replace("**rating**", product.rating);
+      res.end(modifiedProduct);
       break;
     default:
       res.writeHead(404);
