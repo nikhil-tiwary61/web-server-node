e8d168c11ddf6d15bee18afa764a05bb4b974d76
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-08 18:42:10 +0530

    chore: add morgan middleware
    use morgan middleware as logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 
 // const index = fs.readFileSync("index.html", "utf-8");
 // const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
@@ -7,22 +8,25 @@ const express = require("express");
 
 const server = express();
 
+//Third party middlewares
+server.use(morgan("combined"));
+
 // body parser - Inbuilt middleware
 server.use(express.json());
 // server.use(express.urlencoded());
 server.use(express.static("public"));
 
 // Application level middleware
-server.use((req, res, next) => {
-  console.log(
-    req.method,
-    req.ip,
-    req.hostname,
-    new Date(),
-    req.get("User-Agent")
-  );
-  next();
-});
+// server.use((req, res, next) => {
+//   console.log(
+//     req.method,
+//     req.ip,
+//     req.hostname,
+//     new Date(),
+//     req.get("User-Agent")
+//   );
+//   next();
+// });
 
 const auth = (req, res, next) => {
   req.body.password === "123" ? next() : res.sendStatus(401);
