ffe197b7f7b4ac71ef1d2d2c3cbeda749804c867
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-09 17:59:19 +0530

    refactor: reparated routes directly from server to a router

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const express = require("express");
-const morgan = require("morgan");
 const server = express();
-const productController = require("./controller/product");
+const productRouter = require("./routes/product");
 
-//Third party middlewares
-// server.use(morgan("combined"));
 // body parser - Inbuilt middleware
 server.use(express.json());
-
-// MVC - model view controller
-
-server.post("/products", productController.createProduct);
-server.get("/products", productController.getProducts);
-server.get("/products/:id", productController.getProduct);
-server.put("/products/:id", productController.replaceProduct);
-server.patch("/products/:id", productController.updateProduct);
-server.delete("/products/:id", productController.deleteProduct);
+server.use("/api/v1", productRouter.router);
 
 server.listen(8080, () => {
   console.log("server started");
