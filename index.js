9d73959079f1af4b099f43dde21a9e3c61cd049e
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-11 12:01:26 +0530

    feat: connected mongoDB database to node using mongoose

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const server = express();
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
+
+// db connection
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  console.log("database connected");
+}
 
 // body parser - Inbuilt middleware
 server.use(express.json());
