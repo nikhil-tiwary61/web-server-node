cb25567e4e04ad025acf6dfc3237790fe004925b
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-11 11:43:34 +0530

    add .env file for environment variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const server = express();
 const productRouter = require("./routes/product");
@@ -8,6 +9,6 @@ server.use(express.json());
 server.use("/products", productRouter.router);
 server.use("/users", userRouter.router);
 
-server.listen(8080, () => {
+server.listen(process.env.PORT, () => {
   console.log("server started");
 });
