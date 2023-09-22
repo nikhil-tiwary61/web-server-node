75e78b839355a91344606ca9c0843e11393c829a
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-22 23:56:26 +0530

    feat: add multiple parameters to query our database on using req.query

--- controller/product.js
+++ controller/product.js
@@ -42,8 +42,17 @@ exports.createProduct = async (req, res) => {
 
 // Read
 exports.getProducts = async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  let query = Product.find();
+  if (req.query.sort) {
+    const products = await query
+      .sort({ [req.query.sort]: req.query.order })
+      .limit(req.query.limit)
+      .exec();
+    res.json(products);
+  } else {
+    const products = await query.exec();
+    res.json(products);
+  }
 };
 
 exports.getProduct = async (req, res) => {
